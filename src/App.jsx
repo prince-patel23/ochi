bd53a187107406a87566e572e0a827a68d6b92a4
Patel Prince <princeptl2305@gmail.com>
2024-02-24 18:35:15 +0530

    navbar path bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import LandingPage from "./components/LandingPage";
 import Marquee from "./components/Marquee";
 import About from "./components/About";
